e371565f4de2e1f468db81973b2ef7cde6962b78
hamza-aloglu <hamza-aloglu@hotmail.com>
2022-08-15 19:35:28 +0300

    id field added to role_users table.

--- database/migrations/2022_05_22_075818_create_role_users_table.php
+++ database/migrations/2022_05_22_075818_create_role_users_table.php
@@ -14,6 +14,8 @@ return new class extends Migration
     public function up()
     {
         Schema::create('role_users', function (Blueprint $table) {
+            $table->id();
+
             $table->foreignId('role_id')->constrained()->onDelete('cascade');
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
 
